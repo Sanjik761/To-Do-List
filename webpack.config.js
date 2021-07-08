0d5a9f58051e5958b38e474534f53db4f3e23374
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-07-08 07:07:50 +0500

    finish to do lists

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path:path.resolve(__dirname,'dist')
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        contentBase: './dist'
-    }
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: 'as set/resource'
+      }
+    ]
+  }
 }
